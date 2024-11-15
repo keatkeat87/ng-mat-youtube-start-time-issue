4db849f81944392c0af25fd0f6bdfec94869de70
Derrick Yam <keatkeat87@live.com>
2024-11-16 01:22:04 +0800

    fixued

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [YouTubePlayerModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [YouTubePlayerModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   ready = false;
